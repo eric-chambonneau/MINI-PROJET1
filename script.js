1cab692deebd87e8791bc58e4e06c2bd545a77aa
eric-chambonneau <eric.chambonneau@gmail.com>
2022-04-20 22:14:29 +0200

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+const panels = document.querySelectorAll('.container > *')
+
+panels[0].addEventListener('click' , function() {
+    console.log('YEAH');
+});
+
+panels[0].addEventListener('click' , function() {
+    console.log('PAS YEAH');
+});
+
