2ef537d60a6fb312e2ceba64b3be34d39dcc53d1
eric-chambonneau <eric.chambonneau@gmail.com>
2022-04-24 17:34:48 +0200

    cght 1

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const panels = document.querySelectorAll('.container > *')
 
-panels[0].addEventListener('click' , function() {
-    console.log('YEAH');
-});
+for (let panel of panels) {
+    panel.addEventListener("click" , function() {
+        panels.array.forEach(p => p.classList.remove("active") 
+            
+        );
+        panel.classList.add("active");
+    });
+}
 
-panels[0].addEventListener('click' , function() {
-    console.log('PAS YEAH');
-});
 
